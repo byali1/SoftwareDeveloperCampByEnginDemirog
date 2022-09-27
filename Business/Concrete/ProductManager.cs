f0210f7f159d9c05e6cfef9b9555e5ff3e2fc9ae
Ali Ilgın Kıdır <ali_ilgin@outlook.com>
2022-09-27 11:54:46 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int categoryId)
+        {
+            return _productDal.GetAll(p => p.CategoryId == categoryId);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCategoryDal : ICategoryDal
+    {
+        public List<Category> GetAll(Expression<Func<Category, bool>> filter = null)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return filter == null
+                    ? context.Set<Category>().ToList()
+                    : context.Set<Category>().Where(filter).ToList();
+            }
+        }
+
+        public Category Get(Expression<Func<Category, bool>> filter)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Set<Category>().SingleOrDefault(filter);
+            }
+        }
+
+        public void Add(Category entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Category entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Category entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+    }
+}
